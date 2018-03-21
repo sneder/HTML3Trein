825c1de6f1e4138dcca31ec1bb87760919fd497e
sneder <33893902+sneder@users.noreply.github.com>
2018-03-21 18:13:10 +0100

    Acually Added Assignment 2

--- Paginas/README.txt
+++ Paginas/README.txt
@@ -1,8 +1,3 @@
 Jason Steijn 6219195
 Thomas Mosterd 6280838
 WEBTECH 28 FEB 2018
-
-JOBS:
-1. DOM Created Page
-2. Event Propagation (plot.js onderaan)
-3. Contextual menu (bold-italics-none)
--- Paginas/scripts/knowledgeScript.js
+++ Paginas/scripts/knowledgeScript.js
@@ -25,15 +25,16 @@ class article {
   connectArticleTo(articleToConnectTo, articleName){
     var makeLink = document.createElement("a");
     makeLink.href=articleToConnectTo;
-    var a = document.getElementById(this.title);
-    a.appendChild(makeLink);
+    makeLink.title=articleName;
+    var c = document.getElementById(this.title);
+    c.appendChild(makeLink);
   }
 }
 
 //The article with an image class
 class imgArticle extends article {
   constructor(name, content, image) {
-    super(content, name);
+    super(name, content);
     this.image = image                                  //The image is added as attribute
   }
   createArticle() {
@@ -70,12 +71,23 @@ class vidArticle extends article {
 }
 
 //The eventstarter
+//This has not been fully completed due to a time-limitation involving illness and a short holiday of our collaborators. These are three example Articles properly implemented useing the classes and their methods above.
+//The linking feature does not work with text yet, nor does it work with other articles in this page. The knowledge for adding such a thing is present, but the time is not.
 function start() {
-  let test = new article("Jason is awesome", "Uitleg waarom jason awesome is");
-  test.createArticle();
-  let test2 = new imgArticle("I'm testing", "testing so hard", "https://image.spreadshirtmedia.net/image-server/v1/mp/designs/7152074,width=178,height=178/een-beetje-gay-is-okay.png")
-  test2.createArticle();
-  let test3 = new vidArticle("Sicke Skills", "JASON NOSCOPE EM", "https://www.youtube.com/watch?v=BzTYd-MZKj0")
-  test3.connectArticleTo("The_NS.html", "waddup")
-  test3.createArticle();
+
+  let Article1 = new article("The NS", "Nederlandse Spoorwegen (English: 'Dutch Railways') is the principal passenger railway operator in the Netherlands. NS provides rail services on the Dutch main rail network (hoofdrailnet). The rail infrastructure is maintained by network manager ProRail, which was split off from NS in 2003. Freight services, formerly operated by NS Cargo, merged with the DB Schenker group in 2000. NS runs 4,800 scheduled domestic trains a day, serving 1.1 million passengers.Also, NS provides international rail services from the Netherlands to other European destinations and carries out concessions on some foreign rail markets through its subsidiary Abellio.")
+  Article1.createArticle();
+  Article1.connectArticleTo("The_NS.html", "Source");
+  Article1.connectArticleTo("Trains.html", "Trains")
+
+  let Article2 = new imgArticle("VIRM","Verlengd Inter Regio Materiaal The Enlengthened Inter-Regio Material is a train type that the NS uses as an intercity. VIRM-6 and VIRM-4 are the only ones still currently in use. VIRM-6 is a train with 6 carriages, whereas VIRM-4 is a train with 4 carriages. They can be connected together interchangibaly. For example a VIRM-6 with a VIRM-4 to make a 10 carriage train. Also 3 VIRM-4's or 2 VIRM-6's to make a 12 carriage train. Etc.",  "sources/virm.jpg")
+  Article2.createArticle();
+  Article2.connectArticleTo("The_NS.html", "Source");
+  Article2.connectArticleTo("Trains.html", "Trains")
+
+  let Article3 = new vidArticle("The new Generation sprinter of the NS","The video is in Dutch, because logically the introduction of new Dutch trains doesn't exactly excite a lot of international attention.","https://www.youtube.com/watch?v=GyLhne2B3PU")
+  Article3.createArticle();
+  Article3.connectArticleTo("clips.html", "Source");
+  Article3.connectArticleTo("Trains.html", "Trains")
+
 }
