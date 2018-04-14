86ba595190404676aed5d32bf9a703ec4cd27a3e
thomasmosterd <thomasmosterd123@gmail.com>
2018-04-14 13:55:49 +0200

    Product
    Products are now loaded from databse

---
+++ WebShop/express/myapp/public/js/products.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+        $.getJSON("./data/Trains.json", function(result){
+            $.each(result, function(i, field){
+                $('.product'+i).append(
+                	"<h3>"+ result[i].Name + "</h3>" 
+                	+ '<img src="../Images/' + i + '.jpg" alt="product">' +
+                	"<p>" +"Power in KiloWatts: " + result[i].PowerKW + "</p>" +
+                	"<p>" +"Manufacturer: " + result[i].Manufacturer + "</p>" +
+                	"<p>" +"Year of Construction: " + result[i].YearOfConstruction + "</p>" +
+                	"<p>" +"Price: " + result[i].PriceGRAND + "</p>" +
+                	"<button>Buy Now!</button"
+                	);
+                //alert("succes");
+            });
+        });
+    });
