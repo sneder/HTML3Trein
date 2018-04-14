201ef4e7ede77241e4695d95e58ef549ebfb284f
thomasmosterd <thomasmosterd123@gmail.com>
2018-04-14 12:52:22 +0200

    added test.html
    test.html can be used to test the loading of the data

--- WebShop/express/myapp/Appscripts/saveAsJson.js
+++ WebShop/express/myapp/Appscripts/saveAsJson.js
@@ -3,6 +3,6 @@ const sqlite3 = require('sqlite3');
 const exporter = new SqliteToJson({
 	client: new sqlite3.Database('./Db/webShopDatabase.db')
 })
-exporter.save('Accounts', './data/Accounts.txt', function (err) {});
-exporter.save('Trains', './data/Trains.txt', function(err){});
-exporter.save('Transactions', './data/Transactions.txt', function(err){});
+exporter.save('Accounts', './data/Accounts.json', function (err) {});
+exporter.save('Trains', './data/Trains.json', function(err){});
+exporter.save('Transactions', './data/Transactions.json', function(err){});
