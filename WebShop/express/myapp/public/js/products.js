af78f07bb403b67ce0f0ef945501714bcdf3ff4a
sneder <33893902+sneder@users.noreply.github.com>
2018-04-14 21:14:38 +0200

    Veel files die er wss niet moeten zijn

--- WebShop/express/myapp/public/js/products.js
+++ WebShop/express/myapp/public/js/products.js
@@ -1,18 +1,23 @@
-$(document).ready(function(){
-        $.getJSON("./data/Trains.json", function(result){
-            $.each(result, function(i, field){
-                $('.product').append(
-              		"<div>" +
-                	"<h3>"+ result[i].Name + "</h3>" 
-                	+ '<img src="../Images/' + i + '.jpg" alt="product">' +
-                	"<p>" +"Power in KiloWatts: " + result[i].PowerKW + "</p>" +
-                	"<p>" +"Manufacturer: " + result[i].Manufacturer + "</p>" +
-                	"<p>" +"Year of Construction: " + result[i].YearOfConstruction + "</p>" +
-                	"<p>" +"Price: " + result[i].PriceGRAND + "</p>" +
-                	"<button>Buy Now!</button"
-                	+ "</div>"
-                	);
-                //alert("succes");
-            });
-        });
-    });
+$(document).ready(function () {
+	$.getJSON("./data/Trains.json", function (result) {
+		$.each(result, function (i, field) {
+			$('.product').append(
+				"<div>" +
+				"<h3>" + result[i].Name + "</h3>"
+				+ '<img src="../Images/' + i + '.jpg" alt="product">' +
+				"<p>" + "Power in KiloWatts: " + result[i].PowerKW + "</p>" +
+				"<p>" + "Manufacturer: " + result[i].Manufacturer + "</p>" +
+				"<p>" + "Year of Construction: " + result[i].YearOfConstruction + "</p>" +
+				"<p>" + "Price: " + result[i].PriceGRAND + "</p>" +
+				"<button id=:'" + result[i].Name + "'onclick='buy(" + result[i].Name + ")'>Buy Now!</button"
+				+ "</div>"
+			);
+			console.log(" Product receival succes");
+			console.log("Button made"+ result[i].Name)
+		});
+	});
+});
+
+function buy(product){
+
+} 
