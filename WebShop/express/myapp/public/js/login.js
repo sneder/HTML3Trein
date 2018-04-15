bb5381a58ad999bf5ead168c37113c30c36b8c55
sneder <33893902+sneder@users.noreply.github.com>
2018-04-15 16:51:27 +0200

    Login tiny additions

--- WebShop/express/myapp/public/js/login.js
+++ WebShop/express/myapp/public/js/login.js
@@ -1,3 +1,8 @@
+/*
+This file contains all the functions from the login.html page, 
+aswel as for the createAccount.html page
+*/
+
 //Checking if someone has already logged in, if so take them to their profile, if not take them to the login page
 function loginLoad() {
     if (sessionStorage.user === undefined) {
@@ -11,12 +16,14 @@ function loginLoad() {
 //When someone tries to login, check if their login is valid, if so take them to their profile. If not display an error message
 function submitForm() {
 
+    //Getting some variables established
     var x = document.getElementById("loginForm")
     var username = x.elements["username"].value;
     var password = x.elements["password"].value;
     var succesful = false;
-
     console.log("Username:" + username + " password:" + password);
+
+    //The main function
     $.getJSON("./data/Accounts.json", function (r) {
         console.log(r);
         console.log("Amount of users:" + r.length);
@@ -25,6 +32,7 @@ function submitForm() {
             console.log("Password checking: " + r[i].Password);
             if (username == r[i].UserName && password == r[i].Password) {
                 console.log("login succesful");
+                sessionStorage.user=r[i].UserName
                 succesful = true;
             }
             else {
@@ -34,35 +42,11 @@ function submitForm() {
         console.log(succesful);
         if (succesful) {
             //Open their profile
-            window.open("../pages/profile.html", "_self");
+            window.open("../profile.html", "_self");
         }
         else {
             //Error message
             alert("Invalid login! Email/Password incorrect...");
-        }
-    });
-
-}
-
-//Checking the login credentials
-function checkLogin() {
-    var x = document.getElementById("loginForm")
-    var username = x.elements["username"].value;
-    var password = x.elements["password"].value;
-    console.log("Username:" + username + " password:" + password);
-    $.getJSON("./data/Accounts.json", function (r) {
-        console.log(r);
-        console.log("Amount of users:" + r.length);
-        for (var i = 0; i < r.length; i++) {
-            console.log("Username checking: " + r[i].UserName);
-            console.log("Password checking: " + r[i].Password);
-            if (username == r[i].UserName && password == r[i].Password) {
-                console.log("login succesful");
-                return true;
-            }
-            else {
-                console.log("login failed");
-            }
         }
     });
 
