8ee497e06a45dcbc9f9b6b845e2ae46843e0b76e
thomasmosterd <thomasmosterd123@gmail.com>
2018-04-15 18:37:24 +0200

    server sync

---
+++ WebShop/express/myapp2/myapp/Appscripts/saveAsJson.js
@@ -0,0 +1,8 @@
+const SqliteToJson = require('sqlite-to-json');
+const sqlite3 = require('sqlite3');
+const exporter = new SqliteToJson({
+	client: new sqlite3.Database('./Db/webShopDatabase.db')
+})
+exporter.save('Accounts', './public/data/Accounts.json', function (err) {});
+exporter.save('Trains', './public/data/Trains.json', function(err){});
+exporter.save('Transactions', './public/data/Transactions.json', function(err){});
