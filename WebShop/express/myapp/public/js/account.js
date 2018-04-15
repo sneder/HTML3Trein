3b14b30668e0b0d1c1e76cd2d89161641390f931
sneder <33893902+sneder@users.noreply.github.com>
2018-04-15 22:13:46 +0200

    Account.js=account.kys

--- WebShop/express/myapp/public/js/account.js
+++ WebShop/express/myapp/public/js/account.js
@@ -29,19 +29,21 @@ function createAccount() {
     );
 
     //Throw a fit i've you've forgotten to enter something
-    if(u&&p&&n&&pC&&bD&&e&&g){
+    if (u && p && n && pC && bD && e && g) {
 
     }
-    else{
+    else {
         alert("Not all information entered!")
     }
 
     //
-    var db = new sqlite3.Database("./Db/webShopDatabse");
+    var db = new sqlite.Database(':memory:');
     alert(db);
 
-    var sql = "INSERT INTO Accounts (UserName, Password, Name, PostalCode, Email, BirthDate, Gender) VALUES('"  + u + z + p + z + n + z + p + z + bD + z + e + z + g +"')";
+    var sql = "INSERT INTO Accounts (UserName, Password, Name, PostalCode, Email, BirthDate, Gender) VALUES('" + u + z + p + z + n + z + p + z + bD + z + e + z + g + "')";
     console.log(sql);
+
+    db.run(sql);
 }
 
 //Changing the personal information of the user
