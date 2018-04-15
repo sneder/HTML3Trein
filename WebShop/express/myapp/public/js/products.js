a499bedad6c30ff600a4793919584c23f7546da2
thomasmosterd <thomasmosterd123@gmail.com>
2018-04-15 21:49:18 +0200

    comments added

--- WebShop/express/myapp/public/js/products.js
+++ WebShop/express/myapp/public/js/products.js
@@ -1,5 +1,6 @@
 
 $(document).ready(function () {
+	//some vars to make the data search functions easier to read
 	var p = "<p>"
 	var yoc = "Year Of Construction: "
 	var man = "Manufacturer: "
@@ -14,10 +15,13 @@ $(document).ready(function () {
 	var nh = "</h3>"
 	var b = "<button class='buybutton' id="
 	var b2 = ">Buy Now!</button>"
+
 	$.getJSON("./data/Trains.json", function (r) {
 		$.each(r, function (i, field) {
+			//load the first 10 products
 			if (i < 10) {
 				$('.product').append(
+					//the code that gets the product info from the database and puts it on the page.
 					d + h + r[i].Name + nh +
 					'<img class="trainimg" src="images/' + i + '.jpg" alt="product">' +
 					p + PiK + r[i].PowerKW + np +
@@ -26,10 +30,9 @@ $(document).ready(function () {
 					p + pri + r[i].PriceGRAND + "000" + np +
 					b + i + b2 + nd
 				);
-				console.log(" Product receival succes");
-				console.log("Button made" + r[i].Name)
 			}
 		});
+		//pop-up confirming purchase
 		$(".buybutton").click(function () {
 			buymessage(r[this.id].Name)
 	});
@@ -37,6 +40,7 @@ $(document).ready(function () {
 	$("#more").click(function () {
 		$.getJSON("./data/Trains.json", function (r) {
 			$.each(r, function (i, field) {
+				//load the next 10 products
 				if (i > 9 & i < 20) {
 					$('.moreProduct').append(
 						d + h + r[i].Name + nh +
@@ -53,12 +57,15 @@ $(document).ready(function () {
 				buymessage(r[this.id].Name)
 	});
 		});
+		//remove the more button
 		$("#more").remove();
+		//reveal a new button that can be used to load 10 more products
 		$("#evenmore").css("display", "block")
 	});
 	$("#evenmore").click(function () {
 		$.getJSON("./data/Trains.json", function (r) {
 			$.each(r, function (i, field) {
+				//load the last 10 products
 				if (i > 19) {
 					$('.moreProduct').append(
 						d + h + r[i].Name + nh +
@@ -75,18 +82,25 @@ $(document).ready(function () {
 				buymessage(r[this.id].Name)
 	});
 		});
+		//remove the button
 		$("#evenmore").remove();
 	});
+	//the filter function
 	$("#apply").click(function () {
+		//remove the current products
 		$(".product").remove();
+		//remove the buttons
 		$("#more").remove();
-		$(".moreProduct").remove();
 		$("#evenmore").remove();
+		//remove the div containing 'more' products
+		$(".moreProduct").remove();
+		//make a new div which will contain the filtered products
 		$(".productsection").append(
 			'<div class="product"></div>'
 		)
 		$.getJSON("./data/Trains.json", function (r) {
 			$.each(r, function (i, field) {
+				//this checks if either the value in the filter equals the product value or that the filter is empty
 				if ((r[i].Manufacturer == $("#Man").val() || $("#Man").val() == "default") &
 					(r[i].PowerKW == $("#PKW").val() || $("#PKW").val() == "default") &
 					(r[i].YearOfConstruction == $("#YoC").val() || $("#YoC").val() == "") &
@@ -109,10 +123,12 @@ $(document).ready(function () {
 	});
 		});
 	});
+	//refresh the page and reset any filters
 	$("#reset").click(function () {
 		location.reload();
 	});
 });
+//buy pop-up
 function buymessage(product) {
 	alert("Thanks for buying a "+product + "!")
 };
