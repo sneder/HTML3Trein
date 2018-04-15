23d93ac662d0a4bccdd7b5be8295653a3dba46a9
thomasmosterd <thomasmosterd123@gmail.com>
2018-04-15 13:38:01 +0200

    All filter options done

--- WebShop/express/myapp/public/js/products.js
+++ WebShop/express/myapp/public/js/products.js
@@ -1,23 +1,109 @@
+
 $(document).ready(function () {
-	$.getJSON("./data/Trains.json", function (result) {
-		$.each(result, function (i, field) {
+	var p = "<p>" 
+	var yoc = "Year Of Construction: "
+	var man = "Manufacturer: "
+	var PiK = "Power in KiloWatts: "
+	var pri = "Price: "
+	var np = "</p>"
+	var img1 = '<img class="trainimg" src="../Images/'
+	var img2 = '.jpg" alt="product">'
+	var d = "<div>"
+	var nd = "</div>"
+	var h = "<h3>"
+	var nh = "</h3>"
+	var b = "<button id=:'"
+	var b1 = "'onclick='buy("
+	var b2 = ")'>Buy Now!</button>"
+
+
+	$.getJSON("./data/Trains.json", function (r) {
+		$.each(r, function (i, field) {
+			if (i<10){
 			$('.product').append(
-				"<div>" +
-				"<h3>" + result[i].Name + "</h3>"
-				+ '<img src="../Images/' + i + '.jpg" alt="product">' +
-				"<p>" + "Power in KiloWatts: " + result[i].PowerKW + "</p>" +
-				"<p>" + "Manufacturer: " + result[i].Manufacturer + "</p>" +
-				"<p>" + "Year of Construction: " + result[i].YearOfConstruction + "</p>" +
-				"<p>" + "Price: " + result[i].PriceGRAND + "</p>" +
-				"<button id=:'" + result[i].Name + "'onclick='buy(" + result[i].Name + ")'>Buy Now!</button"
-				+ "</div>"
+				d + h + r[i].Name + nh +
+				img1 + i + img2 +
+				p + PiK + r[i].PowerKW + np +
+				p + man + r[i].Manufacturer + np +
+				p + yoc + r[i].YearOfConstruction + np +
+				p + pri + r[i].PriceGRAND + np +
+				b + r[i].Name + b1 + r[i].Name + b2 + nd
 			);
 			console.log(" Product receival succes");
-			console.log("Button made"+ result[i].Name)
+			console.log("Button made"+ r[i].Name)
+		}
 		});
 	});
-});
+		$("#more").click(function() {
+			$.getJSON("./data/Trains.json", function (r) {
+				$.each(r, function (i, field) {
+				if(i>9 & i<20){										
+					$('.moreProduct').append(
+								d + h + r[i].Name + nh +
+								img1 + i + img2 +
+								p + PiK + r[i].PowerKW + np +
+								p + man + r[i].Manufacturer + np +
+								p + yoc + r[i].YearOfConstruction + np +
+								p + pri + r[i].PriceGRAND + np +
+								b + r[i].Name + b1 + r[i].Name + b2 + nd
+								);
+					};
+				});
+		});
+			$("#more").remove();
+			$("#evenmore").css("display", "block")
+	});
+		$("#evenmore").click(function() {
+			$.getJSON("./data/Trains.json", function (r) {
+				$.each(r, function (i, field) {
+				if(i>19){										
+					$('.moreProduct').append(
+								d + h + r[i].Name + nh +
+								img1 + i + img2 +
+								p + PiK + r[i].PowerKW + np +
+								p + man + r[i].Manufacturer + np +
+								p + yoc + r[i].YearOfConstruction + np +
+								p + pri + r[i].PriceGRAND + np +
+								b + r[i].Name + b1 + r[i].Name + b2 + nd
+								);
+					};
+				});
+		});
+			$("#evenmore").remove();
+	});	
+		$("#apply").click(function() {
+			$(".product").remove();
+			$("#more").remove();
+			$(".productsection").append(
+				'<div class="product"></div>'
+				)
+			$.getJSON("./data/Trains.json", function (r) {
+				$.each(r, function (i, field) {
+				if((r[i].Manufacturer == $("#Man").val() || $("#Man").val() == "default") &
+				   (r[i].PowerKW == $("#PKW").val() || $("#PKW").val() == "default") &
+				   (r[i].YearOfConstruction == $("#YoC").val() || $("#YoC").val() == "") &
+				   (r[i].PriceGRAND > $("#miPr").val() || $("#miPr").val() == "") &
+				   (r[i].PriceGRAND < $("#maPr").val() || $("#maPr").val() == "")
+					)
 
+				{										
+					$('.Product').append(
+								d + h + r[i].Name + nh +
+								img1 + i + img2 +
+								p + PiK + r[i].PowerKW + np +
+								p + man + r[i].Manufacturer + np +
+								p + yoc + r[i].YearOfConstruction + np +
+								p + pri + r[i].PriceGRAND + np +
+								b + r[i].Name + b1 + r[i].Name + b2 + nd
+								);
+					};
+				});
+			});	
+		});
+		$("#reset").click(function() {
+			location.reload();
+		});
+});
 function buy(product){
 
 } 
